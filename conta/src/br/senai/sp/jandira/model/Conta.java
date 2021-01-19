2912bade95ea3cc11a3b9e0929d672397da0a651
Prof. Celso <samuel.a.goulart@gmail.com>
2021-01-19 10:07:02 -0300

    Aula 19-01-2021 Parte 01

---
+++ conta/src/br/senai/sp/jandira/model/Conta.java
@@ -0,0 +1,52 @@
+package br.senai.sp.jandira.model;
+
+public class Conta {
+	
+	// Atributo da classe ou Propriedade da classe
+	public String tipo;
+	private double saldo;
+	public String numero;
+	public String titular;
+	public boolean ativa;
+	public double chequeEspecial;
+	
+	// Métodos diz o que um objeto faz
+	public void depositar(double valorDeposito) {
+		
+		if (valorDeposito < 0) {
+			System.out.println("Valor inválido!!!");
+		} else {
+			saldo += valorDeposito;
+		}
+		
+	}
+	
+	public void mostrarSaldoDaConta() {
+		System.out.println("Saldo: " + saldo);
+		System.out.println("Saldo + Limite: " + (saldo + chequeEspecial));
+	}
+	
+	public double getSaldo(){
+		return saldo;
+	}
+	
+	public void sacar(double valorDoSaque) {
+		//parâmetro
+
+		
+		if ((saldo + chequeEspecial) >= valorDoSaque) {
+			saldo -= valorDoSaque;
+		} else {
+			System.out.println("Saldo insuficiente!");
+		}
+		
+		
+	}
+	
+	public void transferir() {
+		
+	}
+
+}
+
+
