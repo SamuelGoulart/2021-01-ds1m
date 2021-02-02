293179b8ed19cb20d251764550cb87397d3c1f74
Prof. Celso <samuel.a.goulart@gmail.com>
2021-02-02 11:37:40 -0300

    Mensagem qualquer

--- academia_aluno/src/br/academia/App.java
+++ academia_aluno/src/br/academia/App.java
@@ -1,13 +1,27 @@
 package br.academia;
 
-import br.academia.gui.FrameAcademia;
+import java.time.LocalDate;
+
+import br.academia.model.Aluno;
+
+//import br.academia.gui.FrameAcademia;
 
 public class App {
 	
    public static void main(String[] args) {
 	   
-	FrameAcademia tela = new FrameAcademia();
-	tela.criarTela();
-	
+	//FrameAcademia tela = new FrameAcademia();
+	//tela.criarTela();
+	   
+	   Aluno  samuel = new Aluno();
+	    
+	   samuel.setNome("Samuel");
+	   samuel.setPeso(60.0);
+	   samuel.setAltura(1.70);
+	   samuel.getDataNascimentoLocalDate.of(2002, 02, 20));
+	  
+	   System.out.println("***" + samuel.getNome()+ "***");
+	   System.out.println("****"+ samuel.getImc());
+	   System.out.println("****"+ samuel.resultadoImc());
 	}
 }
--- academia_aluno/src/br/academia/model/Aluno.java
+++ academia_aluno/src/br/academia/model/Aluno.java
@@ -1,13 +1,19 @@
 package br.academia.model;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Aluno {
       
 	private String nome;
 	private double peso;
 	private double altura;
-	private String sexo;
-	private int idade;
+	private String genero;
+	private LocalDate dataNascimento;
 	
+   
+	// set vou mandar um valor
+	// get vou pegar um valor
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
@@ -32,18 +38,78 @@ public class Aluno {
 		return this.altura;
 	}
 	
-	public void setSexo(String sexo) {
-		this.sexo = sexo;
+	public void setSexo(String genero) {
+		this.genero = genero;
 	}
-	public String getSexo() {
-		return this.sexo;
+	public String getGenero() {
+		return this.genero;
 	}
 	
-	public void setIdade(int idade) {
-		this.idade = idade;
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
 	}
+	
+	public LocalDate getDataNascimento() {
+		return this.dataNascimento;
+	}
+	
+	// Obter a idade da pessoa
 	
 	public int getIdade() {
-		return this.idade;
+		LocalDate hoje = LocalDate.now();
+	    Period periodo = Period.between(this.dataNascimento, hoje);
+	    return periodo.getYears();
 	}
+	
+	
+	//métodos
+	
+    public double getImc() {
+    	
+    	 return  this.peso /  Math.pow(this.altura, 2);
+    }
+    
+    public String resultadoImc() {
+    	
+    	if(this.getImc() < 18.5) {
+    		return "abaixo do peso";
+    	}
+    	else if (this.getImc() >= 18.5 && this.getImc() <= 24.9) {
+    		return "Peso ideal (parabéns)";
+    	}
+    	else if (this.getImc() >= 25 && this.getImc() <=29.9) {
+    		return "Levemente acima do peso";
+    	}
+    	else if (this.getImc() >= 30 && this.getImc() <34.9) {
+    		return "Obesidade grau 1";
+    	}
+    	else if (this.getImc() >= 35 && this.getImc() <=39.9) {
+    		return "Obesidade grau 2 (Severa)";
+    	}
+    	else if (this.getImc() >40) {
+    		return "Obesidade grau 3";
+    	}
+    	
+    	return "";
+    }
+    
+   public double calcularNdc() {
+    	
+    	if(genero == "M") {
+    		
+    		if(dataNascimento => 18 && dataNascimento <= 30) {
+    			
+    		  return 15.3 * peso + 679;
+    		}
+    	}
+     	else {
+    		
+    	}
+    	
+  
+    }
+    
+     
+    
+
 }
